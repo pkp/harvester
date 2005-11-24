11daeca96be7d06107064fd9a10176662d95fb07
alec <alec>
2005-11-24 20:13:44 +0000

    Added missing initial commit files.

---
+++ classes/core/Request.inc.php
@@ -0,0 +1,438 @@
+<?php
+
+/**
+ * Request.inc.php
+ *
+ * Copyright (c) 2005 The Public Knowledge Project
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @package core
+ *
+ * Class providing operations associated with HTTP requests.
+ * Requests are assumed to be in the format http://host.tld/index.php/<page_name>/<operation_name>/<arguments...>
+ *
+ * $Id$
+ */
+
+// The base script through which all requests are routed
+define('INDEX_SCRIPTNAME', 'index.php');
+
+class Request {
+
+	/**
+	 * Get the Site object.
+	 */
+	function &getSite() {
+		import('site.Site');
+		return new Site();
+	}
+
+	/**
+	 * Perform an HTTP redirect to an absolute or relative (to base system URL) URL.
+	 * @param $url string (exclude protocol for local redirects) 
+	 */
+	function redirect($url) {
+		if (HookRegistry::call('Request::redirect', array(&$url))) {
+			return;
+		}
+		
+		if (!preg_match('!^https?://!i', $url)) {
+			$url = Request::getIndexUrl() . '/' . $url;
+		}
+		header("Location: $url");
+		exit();
+	}
+	
+	/**
+	 * Redirect to the current URL, forcing the HTTPS protocol to be used.
+	 */
+	function redirectSSL() {
+		$url = 'https://' . Request::getServerHost() . Request::getRequestPath();
+		if (HookRegistry::call('Request::redirectSSL', array(&$url))) {
+			return;
+		}
+		Request::redirect($url);
+	}
+	
+	/**
+	 * Redirect to the current URL, forcing the HTTP protocol to be used.
+	 */
+	function redirectNonSSL() {
+		$url = 'http://' . Request::getServerHost() . Request::getRequestPath();
+		if (HookRegistry::call('Request::redirectNonSSL', array(&$url))) {
+			return;
+		}
+		Request::redirect($url);
+	}	
+
+	/**
+	 * Get the base URL of the request (excluding script).
+	 * @return string
+	 */
+	function getBaseUrl() {
+		static $baseUrl;
+		
+		if (!isset($baseUrl)) {
+			$baseUrl = Request::getProtocol() . '://' . Request::getServerHost() . Request::getBasePath();
+			HookRegistry::call('Request::getBaseUrl', array(&$baseUrl));
+		}
+		
+		return $baseUrl;
+	}
+
+	/**
+	 * Get the base path of the request (excluding trailing slash).
+	 * @return string
+	 */
+	function getBasePath() {
+		static $basePath;
+		
+		if (!isset($basePath)) {
+			$basePath = dirname($_SERVER['SCRIPT_NAME']);
+			if ($basePath == '/') {
+				$basePath = '';
+			}
+			HookRegistry::call('Request::getBasePath', array(&$basePath));
+		}
+		
+		return $basePath;
+	}
+
+	/**
+	 * Get the URL to the index script.
+	 * @return string
+	 */
+	function getIndexUrl() {
+		static $indexUrl;
+
+		if (!isset($indexUrl)) {
+			$indexUrl = Request::getBaseUrl() . '/' . INDEX_SCRIPTNAME;
+			HookRegistry::call('Request::getIndexUrl', array(&$indexUrl));
+		}
+
+		return $indexUrl;
+	}
+
+	/**
+	 * Get the URL to the currently selected page (excludes other parameters).
+	 * @return string
+	 */
+	function getPageUrl() {
+		return Request::getIndexUrl();
+	}
+
+	/**
+	 * Get the complete URL to this page, including parameters
+	 * @return string
+	 */
+	function getCompleteUrl() {
+		static $completeUrl;
+
+		if (!isset($completeUrl)) {
+			$queryString = &$_SERVER['QUERY_STRING'];
+			$completeUrl = Request::getRequestUrl() . (!empty($queryString)?"?$queryString":'');
+			HookRegistry::call('Request::getCompleteUrl', array(&$completeUrl));
+		}
+
+		return $completeUrl;
+	}
+
+	/**
+	 * Get the complete URL of the request.
+	 * @return string
+	 */
+	function getRequestUrl() {
+		static $requestUrl;
+		
+		if (!isset($requestUrl)) {
+			$requestUrl = Request::getProtocol() . '://' . Request::getServerHost() . Request::getRequestPath();
+			HookRegistry::call('Request::getRequestUrl', array(&$completeUrl));
+		}
+		
+		return $requestUrl;
+	}
+	
+	/**
+	 * Get the completed path of the request.
+	 * @return string
+	 */
+	function getRequestPath() {
+		static $requestPath;
+		if (!isset($requestPath)) {
+			$requestPath = $_SERVER['SCRIPT_NAME'] . (isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '');
+			HookRegistry::call('Request::getRequestPath', array(&$requestPath));
+		}
+		return $requestPath;
+	}
+	
+	/**
+	 * Get the server hostname in the request.
+	 * @return string
+	 */
+	function getServerHost() {
+		static $serverHost;
+		if (!isset($serverHost)) {
+			$serverHost = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST']
+				: (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST']
+				: (isset($_SERVER['HOSTNAME']) ? $_SERVER['HOSTNAME']
+				: 'localhost'));
+			HookRegistry::call('Request::getServerHost', array(&$serverHost));
+		}
+		return $serverHost;
+	}
+
+	/**
+	 * Get the protocol used for the request (HTTP or HTTPS).
+	 * @return string
+	 */
+	function getProtocol() {
+		static $protocol;
+		if (!isset($protocol)) {
+			$protocol = (!isset($_SERVER['HTTPS']) || strtolower($_SERVER['HTTPS']) != 'on') ? 'http' : 'https';
+			HookRegistry::call('Request::getProtocol', array(&$protocol));
+		}
+		return $protocol;
+	}
+
+	/**
+	 * Get the remote IP address of the current request.
+	 * @return string
+	 */
+	function getRemoteAddr() {
+		static $ipaddr;
+		if (!isset($remoteAddr)) {
+			if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				$ipaddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else if (isset($_SERVER['REMOTE_ADDR'])) {
+				$ipaddr = $_SERVER['REMOTE_ADDR'];
+			}
+			if (!isset($ipaddr) || empty($ipaddr)) {
+				$ipaddr = getenv('REMOTE_ADDR');
+			}
+			if (!isset($ipaddr) || $ipaddr == false) {
+				$ipaddr = '';
+			}
+			HookRegistry::call('Request::getRemoteAddr', array(&$ipaddr));
+		}
+		return $ipaddr;
+	}
+	
+	/**
+	 * Get the remote domain of the current request
+	 * @return string
+	 */
+	function getRemoteDomain() {
+		static $remoteDomain;
+		if (!isset($remoteDomain)) {
+			$remoteDomain = getHostByAddr(Request::getRemoteAddr());
+			HookRegistry::call('Request::getRemoteDomain', array(&$remoteDomain));
+		}
+	}
+	
+	/**
+	 * Get the user agent of the current request.
+	 * @return string
+	 */
+	function getUserAgent() {
+		static $userAgent;
+		if (!isset($userAgent)) {
+			if (isset($_SERVER['HTTP_USER_AGENT'])) {
+				$userAgent = $_SERVER['HTTP_USER_AGENT'];
+			}
+			if (!isset($userAgent) || empty($userAgent)) {
+				$userAgent = getenv('HTTP_USER_AGENT');
+			}
+			if (!isset($userAgent) || $userAgent == false) {
+				$userAgent = '';
+			}
+			HookRegistry::call('Request::getUserAgent', array(&$userAgent));
+		}
+		return $userAgent;
+	}
+	
+	/**
+	 * Get the user session associated with the current request.
+	 * @return Session
+	 */
+	 function &getSession() {
+	 	static $session;
+	 	
+	 	if (!isset($session)) {
+	 		$sessionManager = &SessionManager::getManager();
+	 		$session = $sessionManager->getUserSession();
+	 	}
+	 	
+	 	return $session;
+	 }
+	
+	/**
+	 * Get the user associated with the current request.
+	 * @return User
+	 */
+	 function &getUser() {
+	 	static $user;
+	 	
+	 	if (!isset($user)) {
+	 		$sessionManager = &SessionManager::getManager();
+	 		$session = &$sessionManager->getUserSession();
+	 		$user = $session->getUser();
+	 	}
+	 	
+	 	return $user;
+	 }
+	 
+	/**
+	 * Get the page requested in the URL.
+	 * @return String the page path (under the "pages" directory)
+	 */
+	function getRequestedPage() {
+		static $page;
+		
+		if (!isset($page)) {
+			$page = '';
+			if (isset($_SERVER['PATH_INFO'])) {
+				$vars = explode('/', $_SERVER['PATH_INFO']);
+				if (count($vars) >= 2) {
+					$page = Core::cleanFileVar($vars[1]);
+				}
+			}
+		}
+		
+		return $page;
+	}
+	
+	/**
+	 * Get the operation requested in the URL (assumed to exist in the requested page handler).
+	 * @return string
+	 */
+	function getRequestedOp() {
+		static $op;
+		
+		if (!isset($op)) {
+			$op = '';
+			if (isset($_SERVER['PATH_INFO'])) {
+				$vars = explode('/', $_SERVER['PATH_INFO']);
+				if (count($vars) >= 3) {
+					$op = Core::cleanFileVar($vars[2]);
+				}
+			}
+			$op = empty($op) ? 'index' : $op;
+		}
+		
+		return $op;
+	}
+	
+	/**
+	 * Get the arguments requested in the URL (not GET/POST arguments, only arguments prepended to the URL separated by "/").
+	 * @return array
+	 */
+	function getRequestedArgs() {
+		$args = array();
+		if (isset($_SERVER['PATH_INFO'])) {
+			$vars = explode('/', $_SERVER['PATH_INFO']);
+			if (count($vars) > 2) {
+				$args = array_slice($vars, 3);
+				for ($i=0, $count=count($args); $i<$count; $i++) {
+					$args[$i] = Core::cleanVar(get_magic_quotes_gpc() ? stripslashes($args[$i]) : $args[$i]);
+				}
+			}
+		}
+		return $args;	
+	}
+	
+	/**
+	 * Get the value of a GET/POST variable.
+	 * @return mixed
+	 */
+	function getUserVar($key) {
+		static $vars;
+		
+		if (!isset($vars)) {
+			$vars = array_merge($_GET, $_POST);
+		}
+		
+		if (isset($vars[$key])) {
+			// FIXME Do not clean vars again if function is called more than once?
+			Request::cleanUserVar($vars[$key]);
+			return $vars[$key];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Get the value of a GET/POST variable generated using the Smarty
+	 * html_select_date and/or html_select_time function.
+	 * @param $prefix string
+	 * @param $defaultDay int
+	 * @param $defaultMonth int
+	 * @param $defaultYear int
+	 * @param $defaultHour int
+	 * @param $defaultMinute int
+	 * @param $defaultSecond int
+	 * @return Date
+	 */
+	function getUserDateVar($prefix, $defaultDay = null, $defaultMonth = null, $defaultYear = null, $defaultHour = 0, $defaultMinute = 0, $defaultSecond = 0) {
+		$monthPart = Request::getUserVar($prefix . 'Month');
+		$dayPart = Request::getUserVar($prefix . 'Day');
+		$yearPart = Request::getUserVar($prefix . 'Year');
+		$hourPart = Request::getUserVar($prefix . 'Hour');
+		$minutePart = Request::getUserVar($prefix . 'Minute');
+		$secondPart = Request::getUserVar($prefix . 'Second');
+
+		if (empty($dayPart)) $dayPart = $defaultDay;
+		if (empty($monthPart)) $monthPart = $defaultMonth;
+		if (empty($yearPart)) $yearPart = $defaultYear;
+		if (empty($hourPart)) $hourPart = $defaultHour;
+		if (empty($minutePart)) $minutePart = $defaultMinute;
+		if (empty($secondPart)) $secondPart = $defaultSecond;
+
+		if (empty($monthPart) || empty($dayPart) || empty($yearPart)) return null;
+		return mktime($hourPart, $minutePart, $secondPart, $monthPart, $dayPart, $yearPart);
+	}
+
+	/**
+	 * Sanitize a user-submitted variable (i.e., GET/POST/Cookie variable).
+	 * Strips slashes if necessary, then sanitizes variable as per Core::cleanVar().
+	 * @param $var mixed
+	 * @param $stripHtml boolean optional, will encode HTML if set to true
+	 */
+	function cleanUserVar(&$var, $stripHtml = false) {
+		if (isset($var) && is_array($var)) {
+			array_walk($var, create_function('&$item,$key', 'Request::cleanUserVar($item, ' . ($stripHtml ? 'true' : 'false') . ');'));
+		
+		} else if (isset($var)) {
+			$var = Core::cleanVar(get_magic_quotes_gpc() ? stripslashes($var) : $var, $stripHtml);
+			
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Get the value of a cookie variable.
+	 * @return mixed
+	 */
+	function getCookieVar($key) {
+		if (isset($_COOKIE[$key])) {
+			$value = $_COOKIE[$key];
+			Request::cleanUserVar($value);
+			return $value;
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Set a cookie variable.
+	 * @param $key string
+	 * @param $value mixed
+	 */
+	function setCookieVar($key, $value) {
+		setcookie($key, $value, 0, Request::getBasePath());
+		$_COOKIE[$key] = $value;
+	}
+	
+}
+
+?>
