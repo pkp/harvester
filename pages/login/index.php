e667991d7c0c55e979f920d8fd97c03be6ac36d4
jerico.dev <jerico.dev>
2009-12-11 01:58:09 +0000

    #4950# 404 not found when trying to log in in Harvester

---
+++ pages/login/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @defgroup pages_login
+ */
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2003-2009 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @brief Handle login/logout requests.
+ *
+ * @ingroup pages_login
+ */
+
+// $Id$
+
+
+switch ($op) {
+	case 'index':
+	case 'implicitAuthLogin':
+	case 'implicitAuthReturn':
+	case 'signIn':
+	case 'signOut':
+	case 'lostPassword':
+	case 'requestResetPassword':
+	case 'resetPassword':
+	case 'changePassword':
+	case 'savePassword':
+		define('HANDLER_CLASS', 'PKPLoginHandler');
+		import('pages.login.PKPLoginHandler');
+		break;
+}
+
+?>
