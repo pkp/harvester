2cae948909073b4a68d933143c66a24eacb6d6a5
jerico.dev <jerico.dev@gmail.com>
2010-02-24 22:23:45 -0300

    *5179* Migrate build script from CVS to git

--- tools/buildpkg.sh
+++ tools/buildpkg.sh
@@ -13,9 +13,7 @@
 # $Id$
 #
 
-CVSROOT=:pserver:anonymous@pkp.sfu.ca:/cvs
-HARVESTERMODULE=harvester2
-PKPMODULE=pkp
+GITREP=git://github.com/pkp/harvester.git
 
 if [ -z "$1" ]; then
 	echo "Usage: $0 <version> [<tag>] [<patch_dir>]";
@@ -23,7 +21,7 @@ if [ -z "$1" ]; then
 fi
 
 VERSION=$1
-TAG=${2-HEAD}
+TAG=${2-origin/master}
 PATCHDIR=${3-}
 PREFIX=harvester
 BUILD=$PREFIX-$VERSION
@@ -34,31 +32,28 @@ docs/dev							\
 locale/te_ST							\
 plugins/harvesters/junk						\
 tools/buildpkg.sh						\
-tools/genTestLocale.php"
+tools/genTestLocale.php						\
+lib/pkp/tests							\
+.git								\
+lib/pkp/.git"
 
 cd $TMPDIR
 
-echo -n "Exporting $HARVESTERMODULE with tag $TAG ... "
-cvs -Q -d $CVSROOT export -r $TAG -d $BUILD $HARVESTERMODULE || exit 1
-echo "Done"
-
-echo -n "Exporting $PKPMODULE with tag $TAG ... "
-cvs -Q -d $CVSROOT export -r $TAG $PKPMODULE || exit 1
-echo "Done"
-
-if [ ! -d $BUILD/lib ]; then
-	mkdir $BUILD/lib
-fi
-mv $PKPMODULE $BUILD/lib/$PKPMODULE
-
+echo -n "Cloning $GITREP and checking out tag $TAG ... "
+git clone -q -n $GITREP $BUILD || exit 1
 cd $BUILD
+git checkout -q $TAG || exit 1
+echo "Done"
+
+echo -n "Checking out corresponding submodule ... "
+git submodule -q update --init >/dev/null || exit 1
+echo "Done"
 
 echo -n "Preparing package ... "
 cp config.TEMPLATE.inc.php config.inc.php
 mkdir cache/t_cache
-mkdir plugins/postprocessors
-find . -name .cvsignore -exec rm {} \;
-rm -r $EXCLUDE
+find . \( -name .cvsignore -o -name .gitignore -o -name .gitmodules -o -name .keepme \) -exec rm '{}' \;
+rm -rf $EXCLUDE
 echo "Done"
 
 cd ..
