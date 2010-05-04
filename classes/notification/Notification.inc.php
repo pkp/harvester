901f9b7854b8d19ebc57b84e6293bc9cdd12dfe3
Alec Smecher <alec@smecher.bc.ca>
2010-05-04 09:56:24 -0700

    *5234* Added missing notification class

---
+++ classes/notification/Notification.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file classes/notification/Notification.inc.php
+ *
+ * Copyright (c) 2005-2010 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class OJSNotification
+ * @ingroup notification
+ * @see NotificationDAO
+ * @brief Harvester subclass for Notifications
+ */
+
+// $Id$
+
+
+import('lib.pkp.classes.notification.PKPNotification');
+import('lib.pkp.classes.notification.NotificationDAO');
+
+class Notification extends PKPNotification {
+	/**
+	 * Constructor.
+	 */
+	function Notification() {
+		parent::PKPNotification();
+	}
+}
+
+?>
