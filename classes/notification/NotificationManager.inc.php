4a06d98cba325bf720cf6929194740f48f9a22ec
Matthew Crider <mattcrider@gmail.com>
2011-10-27 16:00:53 -0700

    *6934* Stub for NotificationManager

---
+++ classes/notification/NotificationManager.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file classes/notification/NotificationManager.inc.php
+ *
+ * Copyright (c) 2000-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class PKPNotificationManager
+ * @ingroup notification
+ * @see NotificationDAO
+ * @see Notification
+ * @brief Class for Notification Manager.
+ */
+
+
+import('lib.pkp.classes.notification.PKPNotificationManager');
+
+class NotificationManager extends PKPNotificationManager {
+
+	/**
+	 * Constructor.
+	 */
+	function NotificationManager() {
+		parent::PKPNotificationManager();
+	}
+}
+
+?>
