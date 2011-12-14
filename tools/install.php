f4ed44790fe80fbefadcdaa9baf2588477509f83
Alec Smecher <alec@smecher.bc.ca>
2011-12-14 12:53:17 -0800

    *6960* Constructor tune-ups

--- tools/install.php
+++ tools/install.php
@@ -35,7 +35,7 @@ class HarvesterInstallTool extends InstallTool {
 	 * FIXME: Use readline if available?
 	 */
 	function readParams() {
-		AppLocale::requireComponents(array(LOCALE_COMPONENT_PKP_INSTALLER, LOCALE_COMPONENT_APPLICATION_COMMON, LOCALE_COMPONENT_PKP_USER));
+		AppLocale::requireComponents(LOCALE_COMPONENT_PKP_INSTALLER, LOCALE_COMPONENT_APPLICATION_COMMON, LOCALE_COMPONENT_PKP_USER);
 		printf("%s\n", __('installer.harvester2Installation'));
 
 		parent::readParams();
